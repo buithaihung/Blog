458d58897b87cc0cbfe213ca54d39bd21dd7e0b4
buithaihung <buithaihung6934@gmail.com>
2021-12-13 10:32:30 +0000

    add user name and age

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_091634) do
+ActiveRecord::Schema.define(version: 2021_12_13_102754) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,7 +48,10 @@ ActiveRecord::Schema.define(version: 2021_12_13_091634) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
