45156870fc1bfada6a527d36459291ece2c6c452
buithaihung <buithaihung6934@gmail.com>
2021-12-13 11:38:22 +0000

    add user avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-     devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age]
-     devise_parameter_sanitizer.permit :account_update, keys: [:name, :age]
+     devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age,:avatar]
+     devise_parameter_sanitizer.permit :account_update, keys: [:name, :age,:avatar]
     end    
 end 
